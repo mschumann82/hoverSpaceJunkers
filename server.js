c7bf54699c06087a625dda937865dfc058a1dfc1
mschumann82 <37599851+mschumann82@users.noreply.github.com>
2018-08-14 19:48:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require("./controllers/burgers_controller.js");
+const routes = require("./controllers/controller.js");
 
 app.use(routes);
 
